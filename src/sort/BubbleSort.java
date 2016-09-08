8e17707f58d6fb1366a501be53875ee85ee93d2b
khattori <kaha8t@gmail.com>
2016-09-08 10:30:14 +0900

    added BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,49 @@
+package sort;
+
+/*
+ * 隣接するデータの大小を比較し、必要に応じて入れ替える
+ */
+
+public class BubbleSort {
+
+	// 昇順ソート
+	public void ascendingSort(int[] data) {
+		int length = data.length;
+
+		for(int i=0; i<length-1; i++) {
+			for(int j=0; j<length-1-i; j++) {
+				if(data[j] > data[j+1]) {
+					int tmp = data[j];
+					data[j] = data[j+1];
+					data[j+1] = tmp;
+				}
+			}
+		}
+	}
+
+	// 降順ソート
+	public void descendingSort(int[] data){
+		int length = data.length;
+
+		for(int i=0; i<length-1; i++) {
+			for(int j=0; j<length-1-i; j++) {
+				if(data[j] < data[j+1]) {
+					int tmp = data[j];
+					data[j] = data[j+1];
+					data[j+1] = tmp;
+				}
+			}
+		}
+	}
+
+	// 結果表示
+	public void printResult(int[] data){
+		System.out.print("Result of Sorting : [ ");
+
+		for(int i=0; i<data.length; i++) {
+			System.out.print(data[i] + " ");
+		}
+
+		System.out.println("]");
+	}
+}
