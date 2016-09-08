6e97f1d655a5b01bca3d24687ec5c1097c0d14c7
khattori <kaha8t@gmail.com>
2016-09-08 10:46:31 +0900

    added swap method

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import common.Swap;
+
 /*
  * 隣接するデータの大小を比較し、必要に応じて入れ替える
  */
@@ -13,9 +15,7 @@ public class BubbleSort {
 		for(int i=0; i<length-1; i++) {
 			for(int j=0; j<length-1-i; j++) {
 				if(data[j] > data[j+1]) {
-					int tmp = data[j];
-					data[j] = data[j+1];
-					data[j+1] = tmp;
+					Swap.integerSwap(data, j, j+1);
 				}
 			}
 		}
@@ -28,9 +28,7 @@ public class BubbleSort {
 		for(int i=0; i<length-1; i++) {
 			for(int j=0; j<length-1-i; j++) {
 				if(data[j] < data[j+1]) {
-					int tmp = data[j];
-					data[j] = data[j+1];
-					data[j+1] = tmp;
+					Swap.integerSwap(data, j, j+1);
 				}
 			}
 		}
